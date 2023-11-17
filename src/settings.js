d2ba1288d36ab46a5ca307b46961b0d8cf7c2f12
MoRanGu <1126146235@qq.com>
2023-11-17 22:13:35 +0800

    feat(项目初始化): ✨ core-js改为 3.25.6

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,16 @@
 module.exports = {
 
-  title: '黑马人资后台管理系统', //网站标题
+  title: '黑马人资后台管理系统',  //网站标题
 
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
    */
-  fixedHeader: true, //是否固定头部
+  fixedHeader: true,  //是否固定头部
 
   /**
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
-  sidebarLogo: true // 是否显示左侧菜单的Logo
+  sidebarLogo: true  //是否显示左侧菜单的Logo
 }
