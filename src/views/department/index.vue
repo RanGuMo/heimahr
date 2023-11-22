3ad52863103125ae35fc9529908ca58e70177dae
MoRanGu <1126146235@qq.com>
2023-11-22 21:24:51 +0800

    feat(组织架构-节点数据的展示): ✨ 使用默认插槽 动态展示数据

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -5,29 +5,31 @@
       <!-- 展示树形结构 default-expand-all 默认展开 -->
       <el-tree default-expand-all :data="depts" :props="defaultProps">
         <!-- 节点结构 -->
-        <el-row
-          style="width: 100%; height: 40px"
-          type="flex"
-          justify="space-between"
-          align="middle"
-        >
-          <el-col> 传智播客 </el-col>
-          <el-col :span="4">
-            <span class="tree-manager">管理员</span>
-            <el-dropdown>
-              <!-- 显示区域内容 -->
-              <span class="el-dropdown-link">
-                操作<i class="el-icon-arrow-down el-icon--right" />
-              </span>
-              <!-- 下拉菜单选项 -->
-              <el-dropdown-menu slot="dropdown">
-                <el-dropdown-item>添加子部门</el-dropdown-item>
-                <el-dropdown-item>编辑部门</el-dropdown-item>
-                <el-dropdown-item>删除</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown>
-          </el-col>
-        </el-row>
+        <template v-slot="{ node, data }">
+          <el-row
+            style="width: 100%; height: 40px"
+            type="flex"
+            justify="space-between"
+            align="middle"
+          >
+            <el-col>{{ data.name }} </el-col>
+            <el-col :span="4">
+              <span class="tree-manager">{{ data.managerName }}</span>
+              <el-dropdown>
+                <!-- 显示区域内容 -->
+                <span class="el-dropdown-link">
+                  操作<i class="el-icon-arrow-down el-icon--right" />
+                </span>
+                <!-- 下拉菜单选项 -->
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>添加子部门</el-dropdown-item>
+                  <el-dropdown-item>编辑部门</el-dropdown-item>
+                  <el-dropdown-item>删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
       </el-tree>
     </div>
   </div>
@@ -41,10 +43,11 @@ export default {
       depts: [
         {
           name: "传智教育",
+          managerName: "管理员",
           children: [
-            { name: "总裁办" },
-            { name: "行政部" },
-            { name: "财务部" },
+            { name: "总裁办", managerName: "张三" },
+            { name: "行政部", managerName: "李四" },
+            { name: "财务部", managerName: "王五" },
           ],
         },
       ],
