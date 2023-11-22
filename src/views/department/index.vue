fd7cff0b78d4734f9e042cb4adfc9230c161f6c8
MoRanGu <1126146235@qq.com>
2023-11-22 21:17:19 +0800

    feat(组织架构-树形自定义结构): ✨ 使用默认插槽 自定义 树形结构

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,8 +1,34 @@
 <template>
   <div class="container">
     <div class="app-container">
-      <!-- 组织架构   default-expand-all 默认展开-->
-      <el-tree default-expand-all :data="depts" :props="defaultProps"></el-tree>
+      <!-- 组织架构   -->
+      <!-- 展示树形结构 default-expand-all 默认展开 -->
+      <el-tree default-expand-all :data="depts" :props="defaultProps">
+        <!-- 节点结构 -->
+        <el-row
+          style="width: 100%; height: 40px"
+          type="flex"
+          justify="space-between"
+          align="middle"
+        >
+          <el-col> 传智播客 </el-col>
+          <el-col :span="4">
+            <span class="tree-manager">管理员</span>
+            <el-dropdown>
+              <!-- 显示区域内容 -->
+              <span class="el-dropdown-link">
+                操作<i class="el-icon-arrow-down el-icon--right" />
+              </span>
+              <!-- 下拉菜单选项 -->
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>添加子部门</el-dropdown-item>
+                <el-dropdown-item>编辑部门</el-dropdown-item>
+                <el-dropdown-item>删除</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </el-col>
+        </el-row>
+      </el-tree>
     </div>
   </div>
 </template>
@@ -32,7 +58,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.app-container{
+.app-container {
   padding: 30px 140px;
+  font-size: 14px;
+}
+
+.tree-manager {
+  width: 50px;
+  display: inline-block;
+  margin: 10px;
 }
 </style>
