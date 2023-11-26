5c78849e2537eb9ee3849dc078696fd6d5652d01
MoRanGu <1126146235@qq.com>
2023-11-26 17:07:11 +0800

    feat(角色管理-表格自定义结构): ✨ 利用插槽实现自定义结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -9,12 +9,29 @@
       <el-table :data="list">
         <!-- 放置列 -->
         <el-table-column prop="name" align="center" width="200" label="角色" />
-        <el-table-column prop="state" align="center" width="200" label="启用" />
+        <el-table-column prop="state" align="center" width="200" label="启用">
+          <!-- 自定义列结构 -->
+          <template v-slot="{ row }">
+            <span>
+              {{
+                row.state === 1 ? "已启用" : row.state === 0 ? "未启用" : "无"
+              }}
+            </span>
+          </template>
+        </el-table-column>
         <el-table-column prop="description" align="center" label="描述" />
-        <el-table-column align="center" label="操作" />
+        <el-table-column align="center" label="操作">
+          <!-- 放置操作按钮 -->
+          <template>
+            <!-- text 表示为链接 -->
+            <el-button size="mini" type="text">分配权限</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 放置分页组件 -->
-      <el-row type="flex" style="height:60px" align="middle" justify="end">
+      <el-row type="flex" style="height: 60px" align="middle" justify="end">
         <!-- 放置分页组件 -->
         <el-pagination layout="prev, pager, next" />
       </el-row>
@@ -22,26 +39,25 @@
   </div>
 </template>
 <script>
- import { getRoleList } from '@/api/role'
+import { getRoleList } from "@/api/role";
 export default {
-  name: 'Role',
+  name: "Role",
   data() {
     return {
-      list: []
-    }
+      list: [],
+    };
   },
   created() {
-    this.getRoleList()
+    this.getRoleList();
   },
   methods: {
     // 1.角色管理-获取数据
     async getRoleList() {
-      const { rows } = await getRoleList()
-      this.list = rows // 赋值数据
-    }
-  }
-
-}
+      const { rows } = await getRoleList();
+      this.list = rows; // 赋值数据
+    },
+  },
+};
 </script>
 <style scoped>
 .role-operate {
