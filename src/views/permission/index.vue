26a263e97799e03c76ca22312acd1713eff18174
MoRanGu <1126146235@qq.com>
2023-12-08 22:15:00 +0800

    feat(权限管理-获取数据转化树形): ✨ 数据有children并设置row-key展示树形表格

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -2,22 +2,43 @@
   <div class="container">
     <div class="app-container">
       <el-button class="btn-add" size="mini" type="primary">添加权限</el-button>
-      <el-table>
-        <el-table-column label="名称" />
-        <el-table-column label="标识" />
-        <el-table-column label="描述" />
+      <!-- 设置row-key 并且数据存在children字段，就会渲染成树形结构 -->
+      <!-- default-expand-all 默认展开 -->
+      <el-table default-expand-all :data="list" row-key="id">
+        <el-table-column prop="name" label="名称" />
+        <el-table-column prop="code" label="标识" />
+        <el-table-column prop="description" label="描述" />
         <el-table-column label="操作">
-          <el-button size="mini" type="text">添加</el-button>
-          <el-button size="mini" type="text">编辑</el-button>
-          <el-button size="mini" type="text">删除</el-button>
+          <template v-slot="{ row }">
+            <!-- type为1 才有添加按钮 -->
+            <el-button v-if="row.type === 1" size="mini" type="text">添加</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+
         </el-table-column>
       </el-table>
     </div>
   </div>
 </template>
 <script>
+import { getPermissionList } from '@/api/permission'
+import { transListToTreeData } from '@/utils'
 export default {
-  name: 'Permission'
+  name: 'Permission',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      this.list = transListToTreeData(await getPermissionList(), 0) // 将列表数据转化成树形结构
+    }
+  }
 }
 </script>
 <style>
