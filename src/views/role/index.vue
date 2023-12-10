b587265939c5e9f0fe49877f6e805dd91d20c381
MoRanGu <1126146235@qq.com>
2023-12-10 22:45:17 +0800

    feat(权限应用-给角色分配权限-弹出层): ✨ 角色管理-分配权限功能的弹出层显示

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -64,7 +64,9 @@
             <template v-else>
               <!-- 非编辑状态 -->
               <!-- text 表示为链接 -->
-              <el-button size="mini" type="text">分配权限</el-button>
+              <el-button size="mini" type="text" @click="btnPermission"
+                >分配权限</el-button
+              >
               <el-button size="mini" type="text" @click="btnEditRow(row)"
                 >编辑</el-button
               >
@@ -72,7 +74,11 @@
                 title="这是一段内容确定删除吗？"
                 @onConfirm="confirmDel(row.id)"
               >
-                <el-button slot="reference" style="margin-left:10px" size="mini" type="text"
+                <el-button
+                  slot="reference"
+                  style="margin-left: 10px"
+                  size="mini"
+                  type="text"
                   >删除</el-button
                 >
               </el-popconfirm>
@@ -141,11 +147,24 @@
         </el-form-item>
       </el-form>
     </el-dialog>
+
+    <!-- 放置权限弹层 -->
+    <el-dialog :visible.sync="showPermissionDialog" title="分配权限">
+      <!-- 放置权限数据 -->
+      <el-tree
+        :data="permissionData"
+        :props="{ label: 'name' }"
+        show-checkbox
+        default-expand-all
+      />
+    </el-dialog>
   </div>
 </template>
 <script>
 import { getRoleList, addRole, updateRole, delRole } from "@/api/role";
 import role from "@/router/modules/role";
+import {getPermissionList} from '@/api/permission'
+import { transListToTreeData } from "@/utils";
 export default {
   name: "Role",
   data() {
@@ -170,6 +189,8 @@ export default {
           { required: true, message: "角色描述不能为空", trigger: "blur" },
         ],
       },
+      showPermissionDialog: false, //控制权限弹出层的显示和隐藏
+      permissionData: [], //接收权限数的数据
     };
   },
   created() {
@@ -244,14 +265,19 @@ export default {
       }
     },
     // 删除角色
-    async  confirmDel(id) {
-      await delRole(id) // 后端删除
-      this.$message.success('删除角色成功')
+    async confirmDel(id) {
+      await delRole(id); // 后端删除
+      this.$message.success("删除角色成功");
       // 删除的如果是最后一个，让当前页码减一(注意！！！，list指的是当前页面的数据，不是所有的数据)
       // 并且当前页数要大于1
-      if (this.list.length === 1 && this.pageParams.page > 1) this.pageParams.page--
-      this.getRoleList() // 重新获取数据
-}
+      if (this.list.length === 1 && this.pageParams.page > 1)
+        this.pageParams.page--;
+      this.getRoleList(); // 重新获取数据
+    },
+    async btnPermission() {
+      this.permissionData = transListToTreeData(await getPermissionList(), 0);
+      this.showPermissionDialog = true;
+    },
   },
 };
 </script>
